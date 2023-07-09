44c729b12573e8cc126f39127c6a95f7c6a30e46
efecagrisen <efecagrisen@yahoo.com>
2023-07-09 20:48:22 +0300

    lombok

---
+++ m2-enum/src/main/java/com/cydeo/review/EnumTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.review;
+
+public class EnumTest {
+    public static void main(String[] args) {
+
+        User employee = new User("ecs","efe c","sen",Roles.EMPLOYEE);
+        System.out.println(employee);
+
+
+    }
+}
--- m2-enum/src/main/java/com/cydeo/review/Roles.java
+++ m2-enum/src/main/java/com/cydeo/review/Roles.java
@@ -1,5 +1,11 @@
 package com.cydeo.review;
 
 public enum Roles {
-    ADMIN,MANAGER,EMPLOYEE
+    ADMIN ("Admin"),MANAGER ("Manager"),EMPLOYEE("Employee"); // new Role ("Employee")
+
+    public String value;
+
+    Roles(String value) {
+        this.value = value;
+    }
 }
