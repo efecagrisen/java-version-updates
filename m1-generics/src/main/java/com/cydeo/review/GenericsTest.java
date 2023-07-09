e244ed21e7c87088ab9fbedf100a1a9e2cd3cf20
efecagrisen <efecagrisen@yahoo.com>
2023-07-09 19:14:07 +0300

    printList method standard way

--- m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
+++ m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
@@ -13,9 +13,22 @@ public class GenericsTest {
 
 
         List<Teacher> teacherList = new ArrayList<>();
+
         teacherList.add(new Teacher("Albus", 201));
         teacherList.add(new Teacher("Severus", 202));
 
+        printList(studentList);
 
     }
+
+    // write a method that takes a list of stuents prints each students and total sturdent type
+
+    public static void printList (List<Student> studentList){
+        for(Student student : studentList){
+            System.out.println(student);
+            }
+        System.out.println("Total students: "+studentList.size());
+    }
+
+
 }
