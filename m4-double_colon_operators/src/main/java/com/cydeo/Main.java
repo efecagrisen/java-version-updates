c4333992acfed52104c1d8e81aee8dbdaed1b06a
efecagrisen <efecagrisen@yahoo.com>
2023-07-10 21:11:24 +0300

    double colon introduction

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -10,6 +10,9 @@ Calculate sum = (x,y) -> System.out.println(x+y);
 
 Calculate s1 = (x,y) -> Calculator.findSum(x,y);
 
+// Reference to static method
+Calculate s2 = Calculator ::findSum;
+s2.calculate(10,30);
 
     }
 }
