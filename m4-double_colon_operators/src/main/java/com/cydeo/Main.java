300efcf42ea52147a32c0159c36f2d8d058a3ea0
efecagrisen <efecagrisen@yahoo.com>
2023-07-10 20:47:40 +0300

    double colon module created

---
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+Calculate sum = (x,y) -> System.out.println(x+y);
+
+
+Calculate s1 = (x,y) -> Calculator.findSum(x,y);
+
+
+    }
+}
