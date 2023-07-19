16731c00862bef21c6bd9964aa2d53fb50ab0e6e
efecagrisen <efecagrisen@yahoo.com>
2023-07-19 19:37:04 +0300

    m7-optional-bigDecimal created

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import com.cydeo.task.Dish;
 import com.cydeo.task.DishData;
+import com.cydeo.task.Type;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -48,11 +49,40 @@ public class CollectorsDemo {
 
 // 2:01:30
 
+        // counting(): returns a Collector interface that counts the number of elements
+        Long evenCount = numbers.stream()
+                .filter(x->x%2==0)
+                .collect(Collectors.counting());
+        System.out.println(evenCount);
+
+        //averagingInt(ToIntFunction) : returns the average of the integer values
+        Double calorieAverage = DishData.getAll().stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+        System.out.println(calorieAverage);
+
+        //joining() : is used to join various elements of a character or string array into a single object
+        List<String> courses = Arrays.asList("Java","JS","TS");
+        String str = courses.stream()
+                .collect(Collectors.joining(","));
+        System.out.println(str); //Java,JS,TS
+
+        //partitioningBy() : is used to partition a stream of objects (or a set of elements ) based on a given predicate
+        Map<Boolean,List<Dish>> veggieDish = DishData.getAll().stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+        System.out.println(veggieDish);
+        //false dishes >> {false=[Dish(name=pork, vegetarian=false, calories=800, type=MEAT), Dish(name=beef, vegetarian=false, calories=700, type=MEAT), Dish(name=chicken, vegetarian=false, calories=400, type=MEAT), Dish(name=prawns, vegetarian=false, calories=300, type=FISH), Dish(name=salmon, vegetarian=false, calories=450, type=FISH)],
+        //true dishes >> true=[Dish(name=fries, vegetarian=true, calories=530, type=OTHER), Dish(name=rice, vegetarian=true, calories=350, type=OTHER), Dish(name=fruit, vegetarian=true, calories=120, type=OTHER), Dish(name=pizza, vegetarian=true, calories=550, type=OTHER)]}
 
 
-
-
-
+        //groupingBy() : is used for grouping objects by some property and storing result in a map instance
+        Map<Type, List<Dish>> dishType = DishData.getAll().stream()
+                .collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(dishType);
+        /** result: the dishes according to their types >> Fish, Meat, Other
+         *{FISH=[Dish(name=prawns, vegetarian=false, calories=300, type=FISH), Dish(name=salmon, vegetarian=false, calories=450, type=FISH)],
+         *OTHER=[Dish(name=fries, vegetarian=true, calories=530, type=OTHER), Dish(name=rice, vegetarian=true, calories=350, type=OTHER), Dish(name=fruit, vegetarian=true, calories=120, type=OTHER), Dish(name=pizza, vegetarian=true, calories=550, type=OTHER)],
+         *MEAT=[Dish(name=pork, vegetarian=false, calories=800, type=MEAT), Dish(name=beef, vegetarian=false, calories=700, type=MEAT), Dish(name=chicken, vegetarian=false, calories=400, type=MEAT)]}
+         */
 
 
 
