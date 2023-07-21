967d49f2f11885924565ffdb2741f2c4b18d7dde
efecagrisen <efecagrisen@yahoo.com>
2023-07-21 19:20:48 +0300

    private methods in interface

--- m8-new-version-updates/src/main/java/module-info.java
+++ m8-new-version-updates/src/main/java/module-info.java
@@ -1,3 +1,11 @@
 module moduleJava9{
     exports  com.cydeo.java9;
+    // modularity is used to encapsulate our packages
         }
+
+/**
+ * be ready to answer:
+ * what is modularity? -> encapsulating our packages
+ * how to create module-info.java file -> right-click on the package > new > module-info.java
+ * what is inside the module-info.java file -> exports ... , requires ...
+ */
